3da5bbda9a293a3fd78d1cc4c0ab77e39b27d080
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2018-10-21 01:22:44 -0400

    Updates

---
+++ js/style.js
@@ -0,0 +1,31 @@
+// JQuery visibilityToggle plug-in
+jQuery.fn.visible = function() {
+    return this.css('visibility', 'visible');
+};
+
+jQuery.fn.invisible = function() {
+    return this.css('visibility', 'hidden');
+};
+
+jQuery.fn.visibilityToggle = function() {
+    return this.css('visibility', function(i, visibility) {
+        return (visibility == 'visible') ? 'hidden' : 'visible';
+    });
+};
+
+$(() => {
+  $("#read-more").on("click", function(){
+    $(this).visibilityToggle();
+    $("#card-container").fadeToggle();
+  });
+  $("#close-icon").on("click", function(){
+    $("#read-more").visibilityToggle();
+    $("#card-container").fadeToggle();
+  });
+
+  $("#nav-mobile li:first-child").addClass("active");
+  $("#nav-mobile li").on("click", function(){
+    $("#nav-mobile li").removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
---
+++ precache-manifest.5930762e7e2bf80fe462ab95191bb5cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kbhuwalka/SSUI-a6/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "14cc5b3ee36b5520177a",
+    "url": "/kbhuwalka/SSUI-a6/static/js/runtime~main.14cc5b3e.js"
+  },
+  {
+    "revision": "e9d3869ff61eb15fea32",
+    "url": "/kbhuwalka/SSUI-a6/static/js/main.e9d3869f.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/kbhuwalka/SSUI-a6/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "e9d3869ff61eb15fea32",
+    "url": "/kbhuwalka/SSUI-a6/static/css/main.650de62d.chunk.css"
+  },
+  {
+    "revision": "284ac6ad8c9224cd9d40e88f5f573073",
+    "url": "/kbhuwalka/SSUI-a6/index.html"
+  }
+];
